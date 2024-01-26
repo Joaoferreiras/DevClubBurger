eda899bd0295704585f337e2f4226cc7314e1f0d
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-01-26 19:38:06 -0300

    chore: alterando as configurações do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,6 @@
 module.exports = {
     dialect: 'postgres',
-    host:'localhost',
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
+    url: 'postgresql://postgres:b1b51E5d41BG1*1*bC4-cFF1AEDFBC6-@viaduct.proxy.rlwy.net:19691/railway',
     define:{
         timestamps:true,
         underscored:true,
