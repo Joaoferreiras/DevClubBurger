fde656f1184674a372edcfa494260a0598ed6b3a
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-02-19 20:58:25 -0300

    .env

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,9 @@ dotenv.config()
 
 export default{
     dialect: 'postgres',
+    username: process.env.PGUSERNAME,
+    password: process.env.PGUPASSWORD,
+    database: process.env.PGDATABASE,
     url: 'postgresql://postgres:b1b51E5d41BG1*1*bC4-cFF1AEDFBC6-@viaduct.proxy.rlwy.net:19691/railway',
     define:{
         timestamps:true,
