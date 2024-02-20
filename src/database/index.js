dc964b0dd1601208daa94ea86c48d47767944c31
Joaoferreiras <joaovictordesousaferreira1@gmail.com>
2024-02-19 22:01:25 -0300

    postgresUrl

--- src/database/index.js
+++ src/database/index.js
@@ -22,7 +22,7 @@ constructor(){
 
 }
 init(){
-    this.connection = new Sequelize(configDatabase)
+    this.connection = new Sequelize('postgresql://postgres:b1b51E5d41BG1*1*bC4-cFF1AEDFBC6-@viaduct.proxy.rlwy.net:19691/railway')
     models.map((model) => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models))
 
 }
